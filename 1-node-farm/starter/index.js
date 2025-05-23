41e57654bafdabf443609f073a56a1bd3d0bf76b
Hash-D25 <harshita2386@gmail.com>
2025-05-24 00:40:01 +0530

    using modules: our own modules

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -113,23 +113,12 @@ Creating a very simple API
 * fs.readFileSync :is a method that reads the contents of a file synchronously, meaning it will block the execution of the code until the file is read completely.
 */
 
+const replaceTemplate=require('./modules/replaceTemplate');
+
 const tempOverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard=fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%NOT_ORGANIC%}/g, product.organic ? '' : 'not-organic');
-    output = output.replace(/{%ORGANIC%}/g, product.organic ? 'Organic' : 'Not Organic'); // <-- Add this line
-    return output;
-}
 const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj=JSON.parse(data);
 
