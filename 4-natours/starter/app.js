24a92bbaa95752d05eff9b328e515c5552b6eea2
Hash-D25 <harshita2386@gmail.com>
2025-05-27 02:20:16 +0530

    handling patch requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -58,6 +58,26 @@ app.post('/api/v1/tours',(req,res)=>{
     });
 });
 
+app.patch('/api/v1/tours/:id',(req,res)=>{
+    const id=req.params.id*1;
+    const tour=tours.find(el=>el.id===id);
+    if(!tour){
+        return res.status(404).json({
+            status:'fail',
+            message:'Invalid ID'
+        });
+    }
+    Object.assign(tour,req.body);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+        res.status(200).json({
+            status:'success',
+            data:{
+                tour
+            }
+        });
+    });
+});
+
 const port=3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
