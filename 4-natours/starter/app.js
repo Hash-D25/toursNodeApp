bcb30bb76b3200fc1d41c2534c377833d789bc37
Hash-D25 <harshita2386@gmail.com>
2025-05-31 01:56:31 +0530

    environment variales
    -> npm i dotenv
    ->config.env
    ->npm install --save-dev cross-env

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { get } = require('http');
+const tourRouter=require('./routes/tourRoutes');
+const userRouter=require('./routes/userRoutes');
 const app=express();
+const morgan = require('morgan');
+
+// console.log(process.env.NODE_ENV); // 'development' or 'production'
+if(process.env.NODE_ENV==='development'){
+  // If not in development mode, use morgan for logging
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-const tourRouter=require('./routes/tourRoutes');
-const userRouter=require('./routes/userRoutes');
  
 app.use((req,res,next)=>{
     req.requestTime=new Date().toISOString();
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,9 @@
+const dotenv=require('dotenv');
+dotenv.config({path: './config.env'});
 const app=require('./app');
+// console.log(app.get('env')); // 'development' or 'production'
+
+// console.log(process.env); // { NODE_ENV: 'development', PORT: '3000', ... }
 
 const port=3000;
 app.listen(port, () => {
