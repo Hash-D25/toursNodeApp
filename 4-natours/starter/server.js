d4d0a9c53d3bf05bd89eb6c4c8b74cc6d8e08e3d
Hash-D25 <harshita2386@gmail.com>
2025-06-01 19:13:46 +0530

    Setting up esint+prettier in VS code ->Install eslint extension in vscode ->npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,11 +1,21 @@
 const dotenv=require('dotenv');
+const mongoose=require('mongoose');
+
 dotenv.config({path: './config.env'});
 const app=require('./app');
 // console.log(app.get('env')); // 'development' or 'production'
 
 // console.log(process.env); // { NODE_ENV: 'development', PORT: '3000', ... }
+const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+    console.log('🟢DB connection successful! ');
+}).catch(err => {
+    console.error('🔴DB connection error:', err);
+});
+
 
 const port=3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`🟢Server is running on http://localhost:${port}`);
 });
