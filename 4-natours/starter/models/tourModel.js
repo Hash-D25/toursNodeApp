b6ae274f6d75eeef5696227074291ec290e7be17
Hash-D25 <harshita2386@gmail.com>
2025-06-01 22:58:40 +0530

    modelling the tours

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,3 +1,4 @@
+const { ServerDescription } = require('mongodb');
 const mongoose=require('mongoose');
 //Making a schema
 const tourSchema=new mongoose.Schema({
@@ -6,14 +7,50 @@ const tourSchema=new mongoose.Schema({
         required:[true, 'A tour must have a name'],
         unique:true
     },
-    rating:{
+    duration:{
         type:Number,
-        default:4.5,
+        required:[true,'A tour must have a duration']
+    },
+    maxGroupSize:{
+        type:Number,
+        required:[true,'A tour must have a group size'] 
+    },
+    difficulty:{
+        type:String,
+        required:[true,'A tour must have a difficulty']
+    },
+    ratingsAverage:{
+        type:Number,
+        default:4.5
+    },
+    ratingsQuantity:{
+        type:Number,
+        default:0
     },
     price:{
         type:Number,
         required:[true, 'A tour must have a price']
     },
+    priceDiscount:Number,
+    summary:{
+        type:String,
+        trim:true,
+    },
+    description:{
+        type:String,
+        trim:true,
+    },
+    imageCover:{
+        type:String,
+        required:[true,'A tour must have a cover image']
+    },
+    images:[String],
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+        select:false 
+    },
+    startDates:[Date],
 });
 
 //Creating a model
