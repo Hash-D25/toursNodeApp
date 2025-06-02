7aafad532dace3817198558b5cf846aa64745ea3
Hash-D25 <harshita2386@gmail.com>
2025-06-03 00:33:03 +0530

    Aggregation pipelline: Matching and grouping

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -93,3 +93,38 @@ exports.deleteTour=async (req,res)=>{   try{
         });
     }
 };
+
+exports.getTourStats=async (req,res)=>{
+    try{
+        const stats=await Tour.aggregate([
+            {
+                $match:{ratingsAverage:{$gte:4.5}}
+            },
+            {
+                $group:{
+                    _id:{$toUpper:'$ratingsAverage'},
+                    numTours:{$sum:1},
+                    numRatings:{$sum:'$ratingsQuantity'},
+                    avgRating:{$avg:'$ratingsAverage'},
+                    avgPrice:{$avg:'$price'},
+                    minPrice:{$min:'$price'},
+                    maxPrice:{$max:'$price'}
+                }
+            },
+            {
+                $sort:{avgPrice:-1}
+            }
+        ]);
+        res.status(200).json({
+            status:'🟢success',
+            data:{
+                stats
+            }
+        });
+    }catch(err){
+        res.status(404).json({
+            status:'🔴fail',
+            message:err.message
+        });
+    }
+}
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const tourController=require('./../controllers/tourController');
 
 const router=express.Router();
+
+router.route('/tour-stats').get(tourController.getTourStats);
  
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
