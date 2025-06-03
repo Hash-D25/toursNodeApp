a2a312a2cc458956b4d6ab870b34dc20bc02d2dc
Hash-D25 <harshita2386@gmail.com>
2025-06-03 19:14:01 +0530

    document middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,67 +1,84 @@
-const { ServerDescription } = require('mongodb');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 //Making a schema
-const tourSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, 'A tour must have a name'],
-        unique:true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
     },
-    duration:{
-        type:Number,
-        required:[true,'A tour must have a duration']
+    slug:String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
     },
-    maxGroupSize:{
-        type:Number,
-        required:[true,'A tour must have a group size'] 
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
     },
-    difficulty:{
-        type:String,
-        required:[true,'A tour must have a difficulty']
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
     },
-    ratingsAverage:{
-        type:Number,
-        default:4.5
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
     },
-    ratingsQuantity:{
-        type:Number,
-        default:0
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
     },
-    price:{
-        type:Number,
-        required:[true, 'A tour must have a price']
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
     },
-    priceDiscount:Number,
-    summary:{
-        type:String,
-        trim:true,
+    priceDiscount: {
+      type: Number, // Added type: Number
     },
-    description:{
-        type:String,
-        trim:true,
+    summary: {
+      type: String,
+      trim: true,
     },
-    imageCover:{
-        type:String,
-        required:[true,'A tour must have a cover image']
+    description: {
+      type: String,
+      trim: true,
+      type: String, //Fixed typo
     },
-    images:[String],
-    createdAt:{
-        type:Date,
-        default:Date.now(),
-        select:false 
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
     },
-    startDates:[Date],
-}{
-    toJSON:{virtuals:true},
-    toObject:{virtuals:true}
-});
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 //virtual properties
-tourSchema.virtual('durationWeeks').get(function(){
-    return this.duration/7;
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+//document middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
 });
 
 //Creating a model
 // Model is a class with which we can create and read documents from the underlying MongoDB database.
-const Tour=mongoose.model('Tour',tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
 
-module.exports=Tour;
+module.exports = Tour;
