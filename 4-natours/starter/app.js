359700461b5b7a3a5c8f80ede6766fc0df95808e
Hash-D25 <harshita2386@gmail.com>
2025-06-03 23:17:41 +0530

    Better errors and refactoring

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { get } = require('http');
+const AppError = require('./utils/appError.js'); // Corrected path
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
@@ -25,21 +27,10 @@ app.use('/api/v1/users', userRouter);
 
 // Middleware to handle undefined routes
 app.use((req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 //Global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
