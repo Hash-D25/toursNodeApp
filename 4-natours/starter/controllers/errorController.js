dd0ef48eeb82c9cd429ac655deadbd44c9fc04ab
Hash-D25 <harshita2386@gmail.com>
2025-06-04 00:32:55 +0530

    erros during development vs production

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,19 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+const sendErrorProd = (err, res) => {
+    //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } 
+  //Programming or other unknown error: don't leak error details
+  else {
+    console.error('🔴ERROR', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-      stack: err.stack, // Include stack trace for development
-      error: err,
-    });
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    // Simplified error message for production
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
+    sendErrorProd(err, res);
   }
 };
