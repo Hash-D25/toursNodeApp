671a2c03b2bb96cf0c72b4fa2bd42ccaceb2b5ec
Hash-D25 <harshita2386@gmail.com>
2025-06-03 19:52:53 +0530

    custom  data validators -> npm i validator (some more validators library)

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 //Making a schema
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal than 40 characters'],
       minlength: [10, 'A tour name must have more or equal than 10 characters'],
+    //   validate:[validator.isAlpha,'Tour name must only contain characters'],
     },
     slug:String,
     duration: {
@@ -43,7 +45,15 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a price'],
     },
     priceDiscount: {
-      type: Number, // Added type: Number
+      type: Number, 
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator: function (val) {
+          // 'this' only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
     },
     summary: {
       type: String,
