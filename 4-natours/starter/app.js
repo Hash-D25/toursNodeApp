0defdca33cb254f390fafd35d2e3f9d72665777f
Hash-D25 <harshita2386@gmail.com>
2025-06-07 02:14:48 +0530

    setting security http headers : helmet

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError.js'); // Corrected path
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,26 +9,31 @@ const app = express();
 const morgan = require('morgan');
 
 // console.log(process.env.NODE_ENV);
-//GLOBAL MIDDLEWARES
-// 'development' or 'production
+//1) GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   // If not in development mode, use morgan for logging
   app.use(morgan('dev'));
 }
-
+// Limit requests from the same API
 const limiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per windowMs
   windowMs: 60 * 60 * 1000, // 1 hour
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter); // Apply rate limiting to all API routes
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); // Limit request body size to 10kb
 
-app.use(express.json());
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
+  // console.log(req.headers);
   next();
 });
 
