8726a7f897e624670171598e8820a2bcf011e7b3
Hash-D25 <harshita2386@gmail.com>
2025-06-07 23:39:44 +0530

    Prevent parameter pollution

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError.js'); // Corrected path
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
 const morgan = require('morgan');
+const { mongo } = require('mongoose');
 
 // console.log(process.env.NODE_ENV);
 //1) GLOBAL MIDDLEWARES
 
 // Set security HTTP headers
 app.use(helmet());
-// Development lcogging
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   // if not in development mode, use morgan for logging
   app.use(morgan('dev'));
 }
+
 // Limit requests from the same API
 const limiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per windowMs
@@ -25,8 +31,29 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter); // Apply rate limiting to all API routes
+
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); // Limit request body size to 10kb
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ], // Allow these parameters to be duplicated
+  }),
+);
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
