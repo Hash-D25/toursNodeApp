908fee0101828a6cad9fce7e073469839a38bbcc
Hash-D25 <harshita2386@gmail.com>
2025-06-08 15:14:03 +0530

    creating and  getting reviews

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,6 +8,8 @@ const AppError = require('./utils/appError.js'); // Corrected path
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
 const app = express();
 const morgan = require('morgan');
 const { mongo } = require('mongoose');
@@ -66,6 +68,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Middleware to handle undefined routes
 app.use((req, res, next) => {
---
+++ 4-natours/starter/routes/reviewRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const reviewContoller = require('../controllers/reviewController');
+const authController = require('../controllers/authController');
+const router = express.Router();
+
+router
+  .route('/')
+  .get(reviewContoller.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewContoller.createReview,
+  );
+
+module.exports = router;
