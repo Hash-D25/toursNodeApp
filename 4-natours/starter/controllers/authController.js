e80e6915a7d492167d843c401dac27229a36b522
Hash-D25 <harshita2386@gmail.com>
2025-07-20 17:14:41 +0530

    logging in user with out api - part 2

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -61,6 +61,28 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+//Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET,
+    );
+    //1. check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+    //2. check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+    //There is a logged in user
+    res.locals.user = currentUser; // This will be available in the views  (way of passing data to views using the render method)
+  }
+  next();
+});
+
 exports.protect = catchAsync(async (req, res, next) => {
   //1. check if token is provided
   let token;
--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -3,10 +3,11 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
+router.use(authController.isLoggedIn); // Check if user is logged in before rendering views
 
 router.get('/', viewsController.getOverview);
 
-router.get('/tour/:slug', authController.protect, viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
 
 router.get('/login', viewsController.getLoginForm);
 
