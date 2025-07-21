6846a05e8d89de778c836bfb7352db7166786b90
Hash-D25 <harshita2386@gmail.com>
2025-07-21 16:05:21 +0530

    Building the user account page

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -3,12 +3,10 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
-router.use(authController.isLoggedIn); // Check if user is logged in before rendering views
 
-router.get('/', viewsController.getOverview);
-
-router.get('/tour/:slug', viewsController.getTour);
-
-router.get('/login', viewsController.getLoginForm);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 module.exports = router;
