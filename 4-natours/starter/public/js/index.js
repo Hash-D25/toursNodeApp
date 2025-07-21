a9d2dfdc99664bdcfbc1f17b6d7a9a1fa5312a43
Hash-D25 <harshita2386@gmail.com>
2025-07-21 16:51:40 +0530

    Updating user data with our API

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -1,11 +1,13 @@
 import '@babel/polyfill';
 import { login, logout } from './login.js';
+import { updateData } from './updateSettings';
 
 // DOM elements
 
 document.addEventListener('DOMContentLoaded', function () {
   const loginForm = document.querySelector('.form--login');
   const logOutBtn = document.querySelector('.nav__el--logout');
+  const userDataForm = document.querySelector('.form-user-data');
 
   if (loginForm) {
     loginForm.addEventListener('submit', function (e) {
@@ -20,6 +22,15 @@ document.addEventListener('DOMContentLoaded', function () {
     logOutBtn.addEventListener('click', function (e) {
       e.preventDefault();
       logout();
+    });
+  }
+
+  if (userDataForm) {
+    userDataForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const name = document.querySelector('#name').value;
+      const email = document.querySelector('#email').value;
+      updateData(name, email);
     });
   }
 });
